64b19cf01fa3b26c31f6b2e0181377d11ce1a1d6
fuhao <1119369173@qq.com>
2018-03-10 00:25:00 +0800

    创建DefaultPawn，添加门碰撞，添加屋顶，添加墙壁和地面材质，定时关门

--- Source/EscapeTheRoom/Grabber.h
+++ Source/EscapeTheRoom/Grabber.h
@@ -4,18 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-#include "Engine/TriggerVolume.h"
-#include "OpenDoor.generated.h" 
+#include "Grabber.generated.h"
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class ESCAPETHEROOM_API UOpenDoor : public UActorComponent
+class ESCAPETHEROOM_API UGrabber : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
 	// Sets default values for this component's properties
-	UOpenDoor();
+	UGrabber();
 
 protected:
 	// Called when the game starts
@@ -24,19 +23,7 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-        
-public:
-    UPROPERTY(EditAnywhere)
-    ATriggerVolume *Trigger;
-    
-    UPROPERTY(EditAnywhere)
-    float DoorOpenDuration;
 
-private:
-    void OpenDoor();
-    void CloseDoor();
-
-    AActor *Owner;
-    AActor *DefaultPlayer;    
-    float LastDoorOpenTime;
+		
+	
 };
--- Source/EscapeTheRoom/OpenDoor.cpp
+++ Source/EscapeTheRoom/OpenDoor.cpp
@@ -7,31 +7,34 @@
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = true;
 }
 
 
 // Called when the game starts
 void UOpenDoor::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
     Owner = GetOwner();
     DefaultPlayer = GetWorld()->GetFirstPlayerController()->GetPawn();
+    UE_LOG(LogTemp, Warning, TEXT("Hello I am in OpenDoor! %s"), *Owner->GetName());
 }
 
 
 // Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-    if (Trigger->IsOverlappingActor(DefaultPlayer))
+    if (Trigger != nullptr && Trigger->IsOverlappingActor(DefaultPlayer))
     {
         OpenDoor();
+        LastDoorOpenTime = GetWorld()->GetTimeSeconds();
     }
-    else
+
+    if (GetWorld()->GetTimeSeconds() - LastDoorOpenTime > DoorOpenDuration)
     {
         CloseDoor();
     }
